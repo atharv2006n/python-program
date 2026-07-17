3c1e8cb72d3ac922a981fa6911b8738b502ee9c4
atharv2006n <nanavareatharv02@gmail.com>
2026-07-17 15:11:21 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,31 @@
+# # user_data = input("Enter anything: ")
+# # print("Value:", user_data)
+# # print("Data Type:", type(user_)
+    
+# nm="atharv"
+# rn= 75
+# weight= 79
+# boolean=True
+
+# print("Name:",nm, "Type:", type(nm))
+# print("Roll No:",rn, "Type:", type(rn))
+# print("Weight:",weight, "Type:", type(weight))
+# print("Boolean:",boolean, "Type:", type(boolean))
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+age = input("Enter your age: ")
+age = int(age)
+next_year = age + 1
+print("Next year you will be:", next_year)
+
+price = input("Enter item price: ")
+price = float(price)
+total = ((price * 7.5)/100)+price
+print("Total price with tax:", total)
+
+colors = ["Red", "Blue"]
+new_color = input("Enter a new color: ")
+colors.append(new_color)
+print("Your color list:", colors)
